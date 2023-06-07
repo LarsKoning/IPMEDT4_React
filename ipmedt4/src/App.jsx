eceb99b2b28f61b141bb1a0d55172fe7f9b5acfa
lars <koninglars04@gmail.com>
2023-06-07 14:04:08 +0200

    modal gemaakt en gestyled

--- ipmedt4/src/App.jsx
+++ ipmedt4/src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css';
 import Calendar from './components/Calender';
+import Modal from './components/Modal';
+import { useState } from 'react';
 
 
 function App() {
+  const [openModal, setOpenModal] = useState(false) 
+
   return (
     <div className="App">
-      <Calendar />
+      <button className='modalBtn' onClick={() => setOpenModal(true)}>Modal</button>
+      <Modal open={openModal} onClose={() => setOpenModal(false)}/>
+      {/* <Calendar /> */}
     </div>
   );
 }
