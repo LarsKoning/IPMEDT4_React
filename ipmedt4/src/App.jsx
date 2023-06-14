54bebbadddd02221d9fa9cb055cd7c7974bfd3ef
Thijs <s1094272@student.hsleiden.nl>
2023-06-14 11:30:04 +0200

    graph met kleur

--- ipmedt4/src/App.jsx
+++ ipmedt4/src/App.jsx
@@ -4,16 +4,29 @@ import Modal from './components/Modal';
 import { useState } from 'react';
 import TopNavigationBar from './components/TopNavigationBar';
 import BottomNavigationBar from './components/BottomNavigationBar';
+import Graph from './components/Graph';
+import {Earnings} from './Data';
 
 
 function App() {
-  const [openModal, setOpenModal] = useState(false) 
+  const [openModal, setOpenModal] = useState(false)
+  const [earnings, setEarnings] = useState({
+    labels: Earnings.map((data) => data.maand),
+    datasets: [{
+      label: "geld verdiend",
+      data: Earnings.map((data) => data.verdiend),
+      backgroundColor: ["blue", "blue", "blue","blue", "red"]
+    }]
+  })
 
   return (
     <div className="App">
       <TopNavigationBar />
       <button className='modalBtn' onClick={() => setOpenModal(true)}>Tips For More Money</button>
       <Modal open={openModal} onClose={() => setOpenModal(false)}/>
+      <div style={{width: 700, paddingTop: 200}}>
+        <Graph className='graph' chartData={earnings}/>
+      </div>
       {/* <Calendar /> */}
       <BottomNavigationBar />
     </div>
