051b276eff403da5e032598c899078450bdbe135
lars <koninglars04@gmail.com>
2023-06-14 10:58:25 +0200

    Connection backend

--- ipmedt4/src/App.jsx
+++ ipmedt4/src/App.jsx
@@ -1,16 +1,40 @@
 import './App.css';
 import Calendar from './components/Calender';
 import Modal from './components/Modal';
-import { useState } from 'react';
 import  Card  from './components/Card';
 import Rating from './components/Rating';
 import './styles/Card.css';
 import './styles/Rating.css';
 import Tip from './components/Tip';
 import './styles/Tip.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+let d;
 
 
 function App() {
+
+  const [routes, setRoutes] = useState([]);
+  useEffect(() => {
+    async function getAllRoutes(){
+      try {
+        const routes = await axios.get("http://127.0.0.1:8001/api/routes/")
+
+        d = routes.data;
+        setRoutes(routes.data)
+      } catch (error) {
+        console.log(error);
+        
+      }
+    }
+    getAllRoutes()
+    
+  }, [])
+  console.log(d);
+
+
+
   const [openModal, setOpenModal] = useState(false) 
 
   return (
@@ -24,6 +48,12 @@ function App() {
       <Rating header={"Laatste rating"} underText={"5 van de 5"}/>
       </section>
       <Tip text="Je laatste ratings zijn lager dan gemiddeld! Klik hier om tips te krijgen"/>
+
+      {routes.map((reserveringen, i) => {
+        return(
+          <p key={i}>{reserveringen.bedrag}</p>
+        );
+      })}
     </div>
   );
 }
