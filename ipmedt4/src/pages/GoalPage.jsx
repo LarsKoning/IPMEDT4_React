48e7387ce78bbbc9514c84ec46b00fa45310769a
Cas1311 <86728367+Cas1311@users.noreply.github.com>
2023-06-16 16:50:09 +0200

    graph

--- ipmedt4/src/pages/GoalPage.jsx
+++ ipmedt4/src/pages/GoalPage.jsx
@@ -4,11 +4,26 @@ import '../styles/MonthSelect.css'
 import MonthSelect from '../components/MonthSelect';
 import TopNavigationBar from '../components/TopNavigationBar';
 import BottomNavigationBar from '../components/BottomNavigationBar'
-// import Graph from '../components/Graph';
+import Graph from '../components/Graph';
+import Modal from '../components/Modal';
+import { Earnings } from '../Data';
+import { useState } from 'react';
+
 
 
 
 function App() {
+
+  const [openModal, setOpenModal] = useState(false)
+  const [earnings, setEarnings] = useState({
+    labels: Earnings.map((data) => data.maand),
+    datasets: [{
+      label: "geld verdiend",
+      data: Earnings.map((data) => data.verdiend),
+      backgroundColor: ["blue", "blue", "blue", "blue", "red"]
+    }]
+  })
+
   return (
     <div className="App">
       <TopNavigationBar />
@@ -16,7 +31,10 @@ function App() {
         <MonthSelect month={"Juni 2023"} />
         <GoalCard text={"Maand Doel"} amount1="150,00" amount2="200,00" />
       </section>
-      {/* <Graph /> */}
+      <Modal open={openModal} onClose={() => setOpenModal(false)} />
+      <div style={{ width: 700, paddingTop: 200 }}>
+        <Graph className='graph' chartData={earnings} />
+      </div>
       <BottomNavigationBar />
     </div>
   );
