6ca4fd5e7b81f0520ae23829e58a2e7f5436e676
Cas1311 <86728367+Cas1311@users.noreply.github.com>
2023-06-17 12:41:06 +0200

    added routing to home

---
+++ ipmedt4/src/pages/Home.jsx
@@ -0,0 +1,28 @@
+import '../App.css';
+import GoalCard from '../components/GoalCard';
+import '../styles/MonthSelect.css'
+import MonthSelect from '../components/MonthSelect';
+import TopNavigationBar from '../components/TopNavigationBar';
+import BottomNavigationBar from '../components/BottomNavigationBar'
+import Graph from '../components/Graph';
+import Modal from '../components/Modal';
+import { Earnings } from '../Data';
+import { useState } from 'react';
+import Header from '../components/Header';
+
+
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <section className="content">
+        AAA
+      </section>
+
+    </div>
+  );
+}
+
+export default App;
