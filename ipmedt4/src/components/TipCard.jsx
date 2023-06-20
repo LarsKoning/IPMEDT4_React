32a99b9a3f252d80b4b62c67405f7477e9c6604f
Cas1311 <86728367+Cas1311@users.noreply.github.com>
2023-06-20 11:47:52 +0200

    tips done (?)_

--- ipmedt4/src/components/TipCard.jsx
+++ ipmedt4/src/components/TipCard.jsx
@@ -1,38 +1,51 @@
-import React from "react";
-import '../styles/Tips.css'
-import Header from '../components/Header';
+// import React from "react";
+// import "../styles/Tips.css";
 
-const TipCard = () => {
+
+// const TipCard = ({ tipText, tipHead, tipImage }) => {
+//     return (
+//         <div>
+
+//             <section className="tiplijst">
+//                 <section className="tips">
+//                     <img id="tipImage" src={tipImage} alt="Icon" />
+//                     <section>
+//                         <h1>{tipHead}</h1>
+//                         <p>{tipText}</p>
+//                     </section>
+//                 </section>
+//             </section>
+//         </div>
+//     );
+// };
+
+// export default TipCard;
+
+import React from "react";
+import "../styles/Tips.css";
+import PropTypes from "prop-types";
+
+const TipCard = ({ tipText, tipHead, tipImage, className }) => {
     return (
-        <div>
-            <Header text="Overzicht" />
+        <div className={`tipCard ${className}`}>
             <section className="tiplijst">
                 <section className="tips">
-                    <h1>Keyless</h1>
-                    <p>Met SnappCar keyless hoef je niet meer na te denken over de sleuteloverdracht.
-                        Huurders openen je auto volledig beiveiligd met de SnappCar app.
-                    </p>
-                </section>
-                <section className="tips">
-                    <h1>Zet direct boeken aan</h1>
-                    <p>Bepaal zelf precies welke gebruikers jouw auto direct kunnen boeken.
-                        Puur gemak en controle!
-                    </p>
-                </section>
-                <section className="tips">
-                    <h1>Houd je calender up to date</h1>
-                    <p>Door je calender up-to-date te houden, ontvang je alleen verhuurverzoeken die je uitkomen</p>
-                </section>
-                <section className="tips">
-                    <h1>Verhoog het aantal vrije kilometers</h1>
-                    <p>Verhuurders hebben een duidelijke voorkeur voor auto's met een hoger aantal
-                        inbegrepen kilometers, zoals 200 of 300 km.
-                    </p>
+                    <img id="tipImage" src={tipImage} alt="Icon" />
+                    <section>
+                        <h1>{tipHead}</h1>
+                        <p>{tipText}</p>
+                    </section>
                 </section>
             </section>
         </div>
+    );
+};
 
-    )
+TipCard.propTypes = {
+    tipText: PropTypes.string.isRequired,
+    tipHead: PropTypes.string.isRequired,
+    tipImage: PropTypes.string.isRequired,
+    className: PropTypes.string,
 };
 
 export default TipCard;
--- ipmedt4/src/pages/Tips.jsx
+++ ipmedt4/src/pages/Tips.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import '../styles/Tips.css'
 import TipCard from "../components/TipCard";
+import Header from "../components/Header";
+import Keyless from "../icons/keyless.svg";
+import InBook from "../icons/instant-booking.svg";
+import Calendar from "../icons/bigCalendar.svg";
+import Dashboard from "../icons/bigDashboard.svg";
 
 const Tips = () => {
-    return(
-        <TipCard/>
+    return (
+        <div>
+            <Header text="Overzicht" />
+            <TipCard tipImage={Keyless} tipHead={"Keyless"} tipText={"Met SnappCar keyless hoef je niet meer na te denken over de sleuteloverdracht. Huurders openen je auto volledig beiveiligd met de SnappCar app."} />
+            <TipCard tipImage={InBook} tipHead={"Zet direct boeken aan"} tipText={"Bepaal zelf precies welke gebruikers jouw auto direct kunnen boeken. Puur gemak en controle!"} />
+            <TipCard tipImage={Calendar} tipHead={"Houd je calender up to date"} tipText={"Door je calender up-to-date te houden, ontvang je alleen verhuurverzoeken die je uitkomen"} />
+            <TipCard className="bottomCard" tipImage={Dashboard} tipHead={"Verhoog het aantal vrije kilometers"} tipText={"Verhuurders hebben een duidelijke voorkeur voor auto's met een hoger aantal inbegrepen kilometers, zoals 200 of 300 km."} />
+        </div>
     )
-  };
-  
-  export default Tips;
+};
+
+export default Tips;
+
+
