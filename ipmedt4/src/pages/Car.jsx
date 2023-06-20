d34ff88add6a26ae155b45c6d3eb1edd2ae13d42
Cas1311 <86728367+Cas1311@users.noreply.github.com>
2023-06-20 20:37:30 +0200

    small styling

--- ipmedt4/src/pages/Car.jsx
+++ ipmedt4/src/pages/Car.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import '../styles/CarPage.css'
 import Discription from "../components/Discription";
 
-const Car = ({ omschrijving}) => {
-    return (
+const Car = ({ omschrijving }) => {
+  return (
     <div className="caroverzicht">
-      {/* <img src="./icons/car.jpg" alt="car" /> */}
-      <Discription titel={'OPEL'} schakeling={'manual'} vrijekm={100} brandstof={'diesel'} 
-      stoelen={5} type={'hatchback'} jaar={1997} kofferbak={3.5}/>
+      {/* <img src="./icons/car.jpg" alt="car" />
+      <Discription titel={'OPEL'} schakeling={'manual'} vrijekm={100} brandstof={'diesel'}
+        stoelen={5} type={'hatchback'} jaar={1997} kofferbak={3.5} /> */}
     </div>
-    )
-  };        
+  )
+};
 
 
-  
-  export default Car;
+
+export default Car;
