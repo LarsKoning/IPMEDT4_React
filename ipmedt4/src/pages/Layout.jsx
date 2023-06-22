755be5a0567ef3a7ff681ef2156c934680ba1e18
Cas1311 <86728367+Cas1311@users.noreply.github.com>
2023-06-22 13:59:38 +0200

    broke imports

--- ipmedt4/src/pages/Layout.jsx
+++ ipmedt4/src/pages/Layout.jsx
@@ -1,5 +1,6 @@
 import { Outlet, Link } from "react-router-dom";
 import '../styles/BottomNavigationBar.css'
+import '../styles/TopNavigationBar.css'
 
 
 const Layout = () => {
@@ -9,39 +10,39 @@ const Layout = () => {
             <div className="top-navbar">
                 <img src={'./icons/logo.svg'} alt="Logo" />
             </div>
-            
+
             {/* Bottom Navbar */}
             <div >
                 <ul className="bottom-navbar">
                     <Link to='/' >
-                    <li className="selectedPage" >
-                        <img  src={'./icons/dashboard.svg'} alt="dashboard" />
-                        <p>Performance</p>
-                    </li>
+                        <li className="selectedPage" >
+                            <img src={'./icons/dashboard.svg'} alt="dashboard" />
+                            <p>Performance</p>
+                        </li>
                     </Link>
                     <Link to="/car">
-                    <li>
-                        <img src={'./icons/car.svg'} alt="dashboard" />
-                        <p>Car</p>
-                    </li>
+                        <li>
+                            <img src={'./icons/car.svg'} alt="dashboard" />
+                            <p>Car</p>
+                        </li>
                     </Link>
                     <Link className="`navbarLink" to='/Profile'>
-                    <li>
-                        <img src={'./icons/steering-wheel.svg'} alt="dashboard" />
-                        <p>Bookings</p>
-                    </li>
-                        </Link>
-                    <Link className="`navbarLink" to='/Profile'>
-                    <li>
-                        <img src={'./icons/message.svg'} alt="dashboard" />
-                        <p>Messages</p>
-                    </li>
+                        <li>
+                            <img src={'./icons/steering-wheel.svg'} alt="dashboard" />
+                            <p>Bookings</p>
+                        </li>
                     </Link>
                     <Link className="`navbarLink" to='/Profile'>
-                    <li >
-                        <img src={'./icons/profile.svg'} alt="dashboard" />
-                        <p>Profile</p>
-                    </li>
+                        <li>
+                            <img src={'./icons/message.svg'} alt="dashboard" />
+                            <p>Messages</p>
+                        </li>
+                    </Link>
+                    <Link className="`navbarLink" to='/Profile'>
+                        <li >
+                            <img src={'./icons/profile.svg'} alt="dashboard" />
+                            <p>Profile</p>
+                        </li>
                     </Link>
                 </ul>
             </div>
